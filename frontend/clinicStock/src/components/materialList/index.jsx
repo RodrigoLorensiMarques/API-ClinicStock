7e20102796a87410429db648df9d7d6f278274f6
Rodrigo Marques <rodrigolorensimarques@protonmail.com>
2025-02-02 08:56:36 -0300

    feat: cria componente de edição de itens

--- frontend/clinicStock/src/components/materialList/index.jsx
+++ frontend/clinicStock/src/components/materialList/index.jsx
@@ -4,7 +4,9 @@ import trashIcon from "../../assets/trash.svg"
 import AddMaterialForm from "../addMaterialForm";
 import './style.css'
 
-function MaterialList({ searchTerm, materials, loadMaterials}) {
+function MaterialList({ searchTerm, materials, loadMaterials }) {
+    const [editingId, setEditingId] = useState(false);
+    
 
     const hendleDelete = async (id) => {
         try {
@@ -24,19 +26,61 @@ function MaterialList({ searchTerm, materials, loadMaterials}) {
     return (
         <div>
             {filteredMaterials.map((material) => (
-            
-            <div className="item-list">
-                <label>{material.name}</label>
-                <label>{material.id}</label>
-                <label>{material.packaging}</label>
-                <label>{material.amount}</label>
-
-                <div className="container-buttons">
-                    <button className="edit-icon"><i class="fa-solid fa-pen-to-square"></i></button>
-                    <button onClick={() => hendleDelete(material.id)} className="trash-icon"><img src={trashIcon} alt="trashIcon" /></button>
+                <div>
+                    {editingId === material.id ? (
+                        <div className="item-list">
+                            <input
+                                type="text"
+                                placeholder={material.name}
+                                maxLength={40}
+                            />
+                            <label>{material.id}</label>
+                            <input
+                                type="text"
+                                placeholder={material.packaging}
+                                className="input-packaging"
+                            />
+                            <input
+                                type="number"
+                                placeholder={material.amount}
+                                className="input-amout"
+                            />
+    
+                            <div className="container-buttons">
+                                <button
+                                    onClick={() => setEditingId(false)}
+                                    className="check-icon">
+                                    <i class="fa-solid fa-check"></i>
+                                </button>
+                                <button
+                                    onClick={() => hendleDelete(material.id)}
+                                    className="trash-icon">
+                                    <img src={trashIcon} alt="trashIcon" />
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="item-list">
+                            <label><strong>{material.name}</strong></label>
+                            <label>{material.id}</label>
+                            <label>{material.packaging}</label>
+                            <label>{material.amount}</label>
+        
+                            <div className="container-buttons">
+                                    <button
+                                        onClick={() => setEditingId(material.id)}
+                                        className="edit-icon">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </button>
+                                    <button
+                                        onClick={() => hendleDelete(material.id)}
+                                        className="trash-icon">
+                                        <img src={trashIcon} alt="trashIcon" />
+                                    </button>
+                            </div>
+                         </div>
+                    )}
                 </div>
-
-            </div>
             ))}  
         </div>
     )
