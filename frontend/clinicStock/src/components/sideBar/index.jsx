dc1204ff00dbfdf02067734ea260e6decd167472
Rodrigo Marques <rodrigolorensimarques@protonmail.com>
2025-02-05 18:10:49 -0300

    feat: adiciona destaque para a rota atual na SideBar

--- frontend/clinicStock/src/components/sideBar/index.jsx
+++ frontend/clinicStock/src/components/sideBar/index.jsx
@@ -1,17 +1,41 @@
 import React, { useState, useEffect } from "react"
 import healingIcon from "../../assets/healing.svg" 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './style.css'
 
+
+
 function SideBar() {
+    const location = useLocation();
+    const [currentRouth, setCurrentRouth] = useState(location.pathname);
+
+    useEffect(() => {
+    setCurrentRouth(location.pathname);
+}, [location]);
   
     return (
         <div class="side-bar">
             <div class="content-sideBar">
                 <h1>ClinicStock</h1>
                 <div className="links-items">
-                    <Link to="/"><img className="healingIcon" src={healingIcon} draggable="false" />Materiais</Link> 
-                    <Link to="/medicamentos"><span className="medicine-icon" ><i class="fa-solid fa-capsules"></i></span> Medicamentos</Link>
+                    <Link>
+                        <img
+                            className="healingIcon"
+                            src={healingIcon} draggable="false" />
+                        <span
+                            className={`${currentRouth === "/" ? "current-routh" : ""}`} to="/">Materiais
+                        </span>
+                    </Link> 
+
+                    <Link to="/medicamentos">
+                        <span className="medicine-icon" >
+                            <i class="fa-solid fa-capsules"></i>
+                        </span>
+
+                        <span
+                            className={`${currentRouth === "/medicamentos" ? "current-routh" : ""}`} to="/">Medicamentos
+                        </span>
+                    </Link>
                 </div>
             </div>
         </div>
